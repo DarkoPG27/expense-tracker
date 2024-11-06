94c96a6c3fd1d1db3c5f65a81b0bf00fcf90f28a
DarkoPg27 <darkobrnovic27@gmail.com>
2024-11-06 08:21:54 +0100

    transactions in global state

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,28 +1,3 @@
-<script setup>
-const transactions = [
-  {
-    id: 1,
-    text: "Flower",
-    amount: -19.99,
-  },
-  {
-    id: 2,
-    text: "Salary",
-    amount: 299.97,
-  },
-  {
-    id: 3,
-    text: "Book",
-    amount: -10,
-  },
-  {
-    id: 4,
-    text: "Camera",
-    amount: 150,
-  },
-];
-</script>
-
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
